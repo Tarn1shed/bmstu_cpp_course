5ed65fa901c2463e8892f7bfee64349cee3ee2c3
chek1st <artempon6@gmail.com>
2024-10-12 08:36:49 +0300

    add code

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,24 @@
-#include <assert.h>
-#include "str2int.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+
+int main() {
+  char *input = "12345";
+  char *endptr;
+  long int result;
+  result = strtol(input, &endptr, 10);
+  if (errno != 0 || *endptr != '\0') {
+    printf(""); 
+  } else {
+    printf("%ld\n", result);
+  }
+  return 0;
+}
 
 int str2int(const char *str) {
     return 0;
+}
+TEST(str2int, BasicTest) {
+    EXPECT_EQ(str2int("0"), 0);
 }
