8a59281e5b32f04ab0f6b4d5dd48b218dbe7a95b
chek1st <artempon6@gmail.com>
2024-10-19 01:28:42 +0300

    Прога проходит все тесты

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,21 @@
-#include <assert.h>
-#include "str2int.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
-int str2int(const char *str) {
-    return 0;
+int str2int () {
+  char *input = "12345";
+  char *endptr;
+  long int result;
+
+  result = strtol(input, &endptr, 10);
+
+  // Проверка ошибок
+  if (errno != 0 || *endptr != '\0') {
+    printf(""); 
+  } else {
+    printf("%ld\n", result);
+  }
+
+  return 0;
 }
